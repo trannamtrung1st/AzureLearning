585f7ba0b3b33a6a7a9f770228636d158c3db977
trannamtrung1st <trannamtrung1st@gmail.com>
2023-02-10 16:55:59 +0700

    Test

--- Compute/AzureLearning.AzFunctionsIsolated/Function1.cs
+++ Compute/AzureLearning.AzFunctionsIsolated/Function1.cs
@@ -1,5 +1,6 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System.Net;
 
@@ -8,10 +9,17 @@ namespace AzureLearning.AzFunctionsIsolated
     public class Function1
     {
         private readonly ILogger _logger;
+        private string Author { get; }
+        private readonly MyService _myService;
 
-        public Function1(ILoggerFactory loggerFactory)
+        public Function1(
+            ILoggerFactory loggerFactory,
+            IConfiguration configuration,
+            MyService myService)
         {
             _logger = loggerFactory.CreateLogger<Function1>();
+            Author = configuration["Author"];
+            _myService = myService;
         }
 
         [Function("Function1")]
@@ -22,7 +30,7 @@ namespace AzureLearning.AzFunctionsIsolated
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-            response.WriteString("Welcome to Azure Functions!");
+            response.WriteString($"Welcome to Azure Functions! {Author} {_myService.GetValue()}");
 
             return response;
         }
--- Compute/AzureLearning.AzFunctionsIsolated/Program.cs
+++ Compute/AzureLearning.AzFunctionsIsolated/Program.cs
@@ -1,7 +1,22 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices(service =>
+    {
+        service.AddSingleton<MyService>();
+    })
+    .ConfigureAppConfiguration((config) =>
+    {
+        config.AddJsonFile("appsettings.json", optional: true);
+    })
     .Build();
 
 host.Run();
+
+public class MyService
+{
+    public string GetValue() => $"Hello {DateTime.UtcNow}";
+}
